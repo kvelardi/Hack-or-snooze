d7650ed182b851ce194204fdd34b1c8ebc2778cf
Kristen V <124750746+kvelardi@users.noreply.github.com>
2024-01-03 11:55:50 -0500

    initial commit

---
+++ hack-or-snooze-ajax-api/js/nav.js
@@ -0,0 +1,93 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  showPageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+function navSubmitStoryClick(evt) {
+  console.debug("navSubmitStoryClick", evt);
+  hidePageComponents();
+  $submitForm.show();
+}
+
+navSubmitStoryClick.on("click", navSubmitStoryClick);
+
+function navFavoritesClick(evt) {
+  console.debug("navFavoritesClick", evt);
+  hidePageComponents();
+  putFavoritesListOnPage();
+}
+
+$navFavorites.on("click", navFavoritesClick);
+
+function navMyStoriesClick(evt) {
+  console.debug("navMyStoriesClick", evt);
+  hidePageComponents();
+  putUserStoriesOnPage();
+}
+
+$navMyStories.on("click", navMyStoriesClick);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+
+function navUserProfileClick(evt) {
+  console.debug("navUserProfileClick", evt);
+  hidePageComponents();
+  $userProfile.show();
+}
+
+$navUserProfile.on("click", navUserProfileClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+
+
+
+
+
+
+
+
+function navLogOutClick(evt) {
+  console.debug("navLogOutClick", evt);
+  localStorage.clear();
+  location.reload();
+}
+
+$navLogOut.on("click", navLogOutClick);
+
+function navNewLinkClick(evt) {
+  console.debug("navNewLinkClick", evt);
+  hidePageComponents();
+  $submitForm.show();
+}
+
+$navNewLink.on("click", navNewLinkClick);
